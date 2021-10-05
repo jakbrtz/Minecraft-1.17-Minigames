1103b468b427e51ba89056a15e3d91291fabb92a
Jake <jake@bortz.com.au>
2021-10-05 19:06:06 +1100

    Rotated boatrace because I don't know how to rotate the player

--- scripts/server/Games/BoatRace.js
+++ scripts/server/Games/BoatRace.js
@@ -8,10 +8,10 @@ BoatRace = class extends this.Race {
 
 		WorldBuilding.Clear()
 
-		SlashCommand(`/structure load boatrace:boatrace -13 60 -30`)
-		for (var z = -25; z < 25; z++) {
+		SlashCommand(`/structure load boatrace:boatrace -30 60 -13`)
+		for (var x = -25; x < 25; x++) {
 			for (var i = 0; i < 3; i++) {
-				var x = Random.Int(-12, 12)
+				var z = Random.Int(-12, 12)
 				var y = Random.Int(61, 67)
 				SlashCommand(`/fill ${x} 61 ${z} ${x} ${y} ${z} packed_ice`)
 			}
@@ -19,15 +19,15 @@ BoatRace = class extends this.Race {
 	}
 
 	RespawnExtension(player) {
-		SlashCommand(`/tp ${player.name} ${Random.Float(-10, 10)} 66 -28 facing 0 64 0`)
+		SlashCommand(`/tp ${player.name} -28 66 ${Random.Float(-10, 10)} facing 0 64 0`)
 		SlashCommand(`/ride ${player.name} summon_ride boat`)
 	}
 
 	PlayerHasLeftStartArea(player) {
-		return player.position.z >= -25
+		return player.position.x >= -25
 	}
 
 	PlayerIsFinished(player) {
-		return player.position.z >= 25
+		return player.position.x >= 25
 	}
 }
